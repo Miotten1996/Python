434bd899e3c21f814d1946b3678de70a902afd04
shmo <shmo@deloitte.nl>
2022-11-24 14:07:47 +0100

    data

--- My_Project/Data/API_data.py
+++ My_Project/Data/API_data.py
@@ -1,11 +1,19 @@
 import requests
 import pandas as pd
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-r = requests.get(url)
-data = r.json()
-print(data)
+response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
 
+# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
+# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+if response.status_code != 200:
+    raise ValueError("Could not retrieve data, code:", response.status_code)
+
+# The service sends JSON data, we parse that into a Python datastructure
+raw_data = response.json()
+type(raw_data)
+raw_data.keys()
+raw_data['Meta Data']
+time_series = raw_data['Time Series (5min)']
+type(time_series)
 
 
 
